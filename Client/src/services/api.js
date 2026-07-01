107b3ae507ea61cb26191089df0a0683a35df6d8
manthansinghtomar <manthansnghtomar2@gmail.com>
2026-07-01 22:22:56 +0530

    rruri

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://portfolio-full-stack-2026.onrender.com/api'
 });
 
 export default API;
